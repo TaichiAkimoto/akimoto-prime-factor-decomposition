8e94963cfe932bd7fb6119fdef6f3a553273eb59
穐元太一 <taichi@taichis-MacBook-puro.local>
2021-05-03 19:24:36 +0900

    feat: set RCTBridge to call cpp method from iOS through jsi

---
+++ ios/PrimeFactorDecomposition.h
@@ -0,0 +1,9 @@
+#import <React/RCTBridgeModule.h>
+
+#ifdef __cplusplus
+#import "cpp-install.h"
+#endif
+
+@interface PrimeFactorDecomposition : NSObject <RCTBridgeModule>
+@property (nonatomic, assign) BOOL setBridgeOnMainQueue;
+@end
